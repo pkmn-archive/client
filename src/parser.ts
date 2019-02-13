a9f0baa636b4ba3dd1c2963c4cff439166cd1733
Kirk Scheibelhut <kjs@google.com>
2019-02-13 12:09:06 -0800

    Clean up parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,19 +1,44 @@
 /* tslint:disable:switch-default */
 import {ID, toID} from '@pkmn.cc/data';
 
-type Args = [string, ...string[]];
-type KWArgs = {
+export type Args = [string, ...string[]];
+export type KWArgs = {
   [kw: string]: string
 };
 
+const BLOCK_IDS: Set<ID> = new Set([
+  'ingrain',         'quickguard',   'wideguard',      'craftyshield',
+  'matblock',        'protect',      'mist',           'safeguard',
+  'electricterrain', 'mistyterrain', 'psychicterrain', 'telepathy',
+  'stickyhold',      'suctioncups',  'aromaveil',      'flowerveil',
+  'sweetveil',       'disguise',     'safetygoggles',  'protectivepads',
+] as ID[]);
+
+const START_IDS: Set<ID> = new Set([
+  'bind',
+  'wrap',
+  'clamp',
+  'whirlpool',
+  'firespin',
+  'magmastorm',
+  'sandtomb',
+  'infestation',
+  'charge',
+  'trapped',
+] as ID[]);
+
+const MOVE_RELATED_IDS: Set<ID> = new Set(
+    ['spite', 'grudge', 'forewarn', 'sketch', 'leppaberry', 'mysteryberry'] as
+    ID[]);
+
 export class Parser {
+  // istanbul ignore next: constructor
+  protected constructor() {}
+
   static parseLine(line: string): {args: Args, kwArgs: KWArgs} {
-    if (!line.startsWith('|')) {
-      return {args: ['', line], kwArgs: {}};
-    }
-    if (line === '|') {
-      return {args: ['done'], kwArgs: {}};
-    }
+    if (!line.startsWith('|')) return {args: ['', line], kwArgs: {}};
+    if (line === '|') return {args: ['done'], kwArgs: {}};
+
     const index = line.indexOf('|', 1);
     const cmd = line.slice(1, index);
     switch (cmd) {
@@ -53,28 +78,45 @@ export class Parser {
           kwArgs: {},
         };
     }
+
     const args: Args = line.slice(1).split('|') as Args;
     const kwArgs: KWArgs = {};
     while (args.length > 1) {
       const lastArg = args[args.length - 1];
       if (lastArg.charAt(0) !== '[') break;
+
       const bracketPos = lastArg.indexOf(']');
       if (bracketPos <= 0) break;
+
       // default to '.' so it evaluates to boolean true
       kwArgs[lastArg.slice(1, bracketPos)] =
           lastArg.slice(bracketPos + 1).trim() || '.';
       args.pop();
     }
+
     return Parser.upgradeArgs({args, kwArgs});
   }
 
-  static upgradeArgs({args, kwArgs}: {args: Args, kwArgs: KWArgs}):
+  static effectId(effect?: string): ID {
+    if (!effect) return '' as ID;
+
+    if (effect.startsWith('item:') || effect.startsWith('move:')) {
+      effect = effect.slice(5);
+    } else if (effect.startsWith('ability:')) {
+      effect = effect.slice(8);
+    }
+
+    return toID(effect);
+  }
+
+  private static upgradeArgs({args, kwArgs}: {args: Args, kwArgs: KWArgs}):
       {args: Args, kwArgs: KWArgs} {
     switch (args[0]) {
       case '-activate': {
         if (kwArgs.item || kwArgs.move || kwArgs.number || kwArgs.ability) {
           return {args, kwArgs};
         }
+
         const [, pokemon, effect, arg3, arg4] = args;
         const target = kwArgs.of;
         const id = Parser.effectId(effect);
@@ -88,33 +130,14 @@ export class Parser {
           };
         }
 
-        if ([
-              'ingrain',       'quickguard',     'wideguard',
-              'craftyshield',  'matblock',       'protect',
-              'mist',          'safeguard',      'electricterrain',
-              'mistyterrain',  'psychicterrain', 'telepathy',
-              'stickyhold',    'suctioncups',    'aromaveil',
-              'flowerveil',    'sweetveil',      'disguise',
-              'safetygoggles', 'protectivepads',
-            ].includes(id)) {
+        if (BLOCK_IDS.has(id)) {
           return {
             args: ['-block', target || pokemon, effect, arg3],
             kwArgs: {}
           };
         }
 
-        if ([
-              'bind',
-              'wrap',
-              'clamp',
-              'whirlpool',
-              'firespin',
-              'magmastorm',
-              'sandtomb',
-              'infestation',
-              'charge',
-              'trapped',
-            ].includes(id)) {
+        if (START_IDS.has(id)) {
           return {args: ['-start', pokemon, effect], kwArgs: {of: target}};
         }
 
@@ -129,10 +152,7 @@ export class Parser {
         } else if (id === 'skillswap' || id === 'mummy') {
           kwArgs.ability = arg3;
           kwArgs.ability2 = arg4;
-        } else if ([
-                     'spite', 'grudge', 'forewarn', 'sketch', 'leppaberry',
-                     'mysteryberry'
-                   ].includes(id)) {
+        } else if (MOVE_RELATED_IDS.has(id)) {
           kwArgs.move = arg3;
           kwArgs.number = arg4;
         }
@@ -152,16 +172,7 @@ export class Parser {
         // NEW: |-activate||move:Splash
         return {args: ['-activate', '', 'move:Splash'], kwArgs};
     }
-    return {args, kwArgs};
-  }
 
-  static effectId(effect?: string) {
-    if (!effect) return '';
-    if (effect.startsWith('item:') || effect.startsWith('move:')) {
-      effect = effect.slice(5);
-    } else if (effect.startsWith('ability:')) {
-      effect = effect.slice(8);
-    }
-    return toID(effect);
+    return {args, kwArgs};
   }
 }
